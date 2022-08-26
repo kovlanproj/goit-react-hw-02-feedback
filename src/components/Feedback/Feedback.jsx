09625d3ebc02923b9908e54c0bb632d35ab79aa9
kovlanproj <kozorizva@gmail.com>
2022-08-26 14:54:21 +0300

    step 2

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -7,13 +7,57 @@ class Feedback extends Component {
     bad: 0,
   };
 
+  addFeedback = type => {
+    this.setState(prevState => ({ [type]: prevState[type] + 1 }));
+  };
+
+  countTotalFeedbacks = () => {
+    const values = Object.values(this.state);
+
+    let total = 0;
+
+    for (const value of values) {
+      total += value;
+    }
+    return total;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    // console.log(typeof this.state.good);
+    return Math.round((this.state.good / this.countTotalFeedbacks()) * 100);
+  };
+
   render() {
     return (
       <div>
         <h2>Please leave feedback</h2>
-        <button type="button">Good</button>
-        <button type="button">Neutral</button>
-        <button type="button">Bad</button>
+        <button
+          type="button"
+          name="good"
+          onClick={() => this.addFeedback('good')}
+        >
+          Good
+        </button>
+        <button
+          type="button"
+          name="good"
+          onClick={() => this.addFeedback('neutral')}
+        >
+          Neutral
+        </button>
+        <button
+          type="button"
+          name="good"
+          onClick={() => this.addFeedback('bad')}
+        >
+          Bad
+        </button>
+        <h2>Statistics</h2>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
+        <p>Total feedbacks: {this.countTotalFeedbacks()}</p>
+        <p>PositiveFeedback: {this.countPositiveFeedbackPercentage()}%</p>
       </div>
     );
   }
